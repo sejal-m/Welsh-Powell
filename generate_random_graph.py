c3eee3d864b01b991be51e374240dacc10544a34
sejal-m <sejalmaurya27@gmail.com>
2021-10-30 12:14:53 +0530

    generate graph of 100 nodes

--- generate_random_graph.py
+++ generate_random_graph.py
@@ -16,26 +16,19 @@ def ER(n, p):
     g = nx.Graph()
     g.add_nodes_from(V)
     g.add_edges_from(E)
-    
-    for (u, v) in g.edges():
-        g.edges[u,v]['weight'] = random.randint(1,10)
 
     return g
 
 print('hello world')
 
-n = 5
+n = 100
 p = 0.4
 G = ER(n, p)
-python_file = open("example.txt", "w")
-for node1, node2, data in G.edges(data=True):
-    data = str(node1) + ' '+ str(node2) + ' ' + str(data['weight']) + '\n'
+python_file = open("welsh_powell_input.txt", "w")
+for node1, node2 in G.edges():
+    data = str(node1) + ' '+ str(node2) + '\n'
     python_file.write(data)
 
-#for x in G.edges():
-    #qri = str(x)+' '+str(y)+'\n'
-    #python_file.write(qri)
-    #print(x)
 python_file.close()
 
 pos = nx.spring_layout(G)
